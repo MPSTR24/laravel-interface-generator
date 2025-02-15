6f72e064f559ae4e2c904959700da9da0ad8b27d
MPSTR24 <74561948+MPSTR24@users.noreply.github.com>
2025-02-15 14:22:33 +0000

    feat: interface generator command gathers all models

--- src/Console/Commands/InterfaceGenerator.php
+++ src/Console/Commands/InterfaceGenerator.php
@@ -3,6 +3,8 @@
 namespace Mpstr24\InterfaceTyper\Console\Commands;
 
 use Illuminate\Console\Command;
+use ReflectionClass;
+use ReflectionException;
 
 class InterfaceGenerator extends Command
 {
@@ -11,20 +13,39 @@ class InterfaceGenerator extends Command
      *
      * @var string
      */
-    protected $signature = 'app:interface-generator';
+    protected $signature = 'generate:interfaces';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'This command will automatically generate interfaces for your Laravel models based off their migrations';
 
     /**
      * Execute the console command.
+     * @throws ReflectionException
      */
     public function handle()
     {
-        //
+        // Find all model within the project
+
+        $models_path = app_path('Models');
+
+        // remove . .. from results
+        $files = array_diff(scandir($models_path), array('.', '..'));
+
+        foreach ($files as $file) {
+            $this->info($file);
+
+            // get file name only, strip .php
+            $file_name_only = pathinfo($file, PATHINFO_FILENAME);
+
+            // build model path
+            $model_path = 'App\\Models\\'.$file_name_only;
+
+            $model_reflection = new ReflectionClass($model_path);
+            $this->info("Model: " . $model_reflection->getName());
+        }
     }
 }
