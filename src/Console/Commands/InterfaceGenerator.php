5710bf2c5942b43aacbb966dbec18bc80eb991d5
MPSTR24 <74561948+MPSTR24@users.noreply.github.com>
2025-02-15 15:09:39 +0000

    chore: started on creating interfaces from migrations

--- src/Console/Commands/InterfaceGenerator.php
+++ src/Console/Commands/InterfaceGenerator.php
@@ -4,6 +4,8 @@ namespace Mpstr24\InterfaceTyper\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use ReflectionClass;
 use ReflectionException;
 
@@ -33,6 +35,35 @@ class InterfaceGenerator extends Command
 
         foreach ($models as $model) {
             $this->getInterfaceFromFillables($model);
+        }
+
+        // getting the model's columns will provide datatypes instead + id or uuid + created_at/updated_at
+        // if $table->timestamps() is present
+
+        foreach ($models as $model) {
+            // get the current table
+            $table = $model->getTable();
+            $this->info($table);
+
+            // this provides a complete type list compared to fillables
+            $columns = DB::connection()->getSchemaBuilder()->getColumns($table);
+
+
+            $model_interface = "export interface " . class_basename($model) . " { \n";
+
+            // parse returned columns
+            foreach ($columns as $column) {
+                $column_name = $column['name'];
+                $column_type_name = $column['type_name'];
+                $column_type = $column['type'];
+                $column_nullable = $column['nullable'];
+
+                $model_interface .= '   ' . $column_name . ': ' . $column_type_name . ";\n";
+            }
+            $model_interface .= "}\n";
+
+            $this->info($model_interface);
+
         }
 
     }
