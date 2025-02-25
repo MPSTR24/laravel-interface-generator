bac640a1ec353fd3fb8fd1b3cfe51baf91288ae2
MPSTR24 <74561948+MPSTR24@users.noreply.github.com>
2025-02-25 22:41:16 +0000

    chore: began adding code coverage

--- tests/InterfaceGeneratorTest.php
+++ tests/InterfaceGeneratorTest.php
@@ -1,7 +1,19 @@
 <?php
 
-it('generates the correct "TestUser" interface from fillables', function () {
-    $this->artisan('generate:interfaces', ['--mode' => 'fillables'])
-        ->assertExitCode(0)
-        ->expectsOutputToContain("export interface TestUserInterface { \n   first_name: any;\n   last_name: any;\n   email: any;\n}\n");
-});
+namespace Mpstr24\InterfaceTyper\Tests;
+
+
+use PHPUnit\Framework\Attributes\CoversMethod;
+
+#[CoversMethod('Mpstr24\Interfacetyper\Console\Commands\InterfaceGenerator','getInterfaceFromFillables')]
+class InterfaceGeneratorTest extends TestCase
+{
+
+
+    public function testGeneratesCorrectTestUserInterfaceFromFillables()
+    {
+        $this->artisan('generate:interfaces', ['--mode' => 'fillables'])
+            ->assertExitCode(0)
+            ->expectsOutputToContain("export interface TestUserInterface { \n   first_name: any;\n   last_name: any;\n   email: any;\n}\n");
+    }
+}
